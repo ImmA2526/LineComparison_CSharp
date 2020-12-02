4036cf9ef43c2adeb35309c1092dad74d0c876dc
Avenger2627 <imraninfo97@gmail.com>
2020-12-02 15:09:00 +0530

    [Imran Shaikh] Add: Line Compare Check Lines are Equal or not Added

--- Program.cs
+++ Program.cs
@@ -8,15 +8,30 @@ namespace LineComparison
         {
             Console.WriteLine("************Welcome To Line Comparison*******");
             double Length1;
+            double Length2;
             Random Number = new Random();
             double x1 = Number.Next(0, 3);
             double x2 = Number.Next(0, 3);
             double y1 = Number.Next(0, 3);
             double y2 = Number.Next(0, 3);
+            double x3 = Number.Next(0, 3);
+            double x4 = Number.Next(0, 3);
+            double y3 = Number.Next(0, 3);
+            double y4 = Number.Next(0, 3);
             Length1 = (Math.Sqrt((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-            Console.WriteLine("End Points for X1 is : " + x1 + " y1= " + y1);
-            Console.WriteLine("End Points for X2 is : " + x2 + " y2= " + y2);
-            Console.WriteLine("Length of the Line is :" + Length1);
+            Length2 = (Math.Sqrt((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));
+            Console.WriteLine("1st Line Length is : " + Length1);
+            Console.WriteLine("2nd Line Length is : " + Length2);
+
+            if (Length1 == Length2)
+            {
+                Console.WriteLine("Lines are Equal");
+
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
         }
     }
 }
